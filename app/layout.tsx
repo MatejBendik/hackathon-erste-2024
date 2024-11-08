54cecf0c508b1c438c5eab57c03e244175fd0263
MatejBendik <matejbendik.mb@gmail.com>
2024-11-08 17:13:46 +0100

    Add clerk auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,13 @@
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+
 import type { Metadata } from "next";
+
 import localFont from "next/font/local";
 import "./globals.css";
 
@@ -24,12 +33,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <div className="flex justify-end m-4">
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </div>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
