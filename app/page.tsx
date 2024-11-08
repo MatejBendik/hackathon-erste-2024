ff93a172a1b96adc70d5a57dcf3490f2f73a0fbb
MatejBendik <matejbendik.mb@gmail.com>
2024-11-08 17:46:51 +0100

    Add shadcnui lib

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div>
       <main>
-        <h1>
-          Welcome to out App vole ve stylu
-        </h1>
+        <h1>Welcome to out App vole ve stylu</h1>
+        <Button> Button zo shadcn </Button>
       </main>
     </div>
   );
