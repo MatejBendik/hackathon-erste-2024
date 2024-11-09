481b1efa23c39a2166ae6a94efe1d4849957e25d
MiroslavGit <miroslav2022@gmail.com>
2024-11-09 02:32:32 +0100

    fixed ui chat component

--- components/custom/ChatComponent.tsx
+++ components/custom/ChatComponent.tsx
@@ -54,20 +54,18 @@ const ChatComponent = ({ formData, initialAnswer }: ChatComponentProps) => {
 
   return (
     <div className="flex flex-col h-screen p-6 bg-gray-100 dark:bg-gray-800">
-      <div className="flex-grow overflow-y-auto p-4 mb-4 space-y-4 bg-white dark:bg-gray-900 rounded-lg shadow-lg">
+      <div className="flex-grow overflow-y-auto mt-20 p-4 mb-4 space-y-4 bg-white dark:bg-gray-900 rounded-lg shadow-lg">
         {messages.map((message, index) => (
           <div
             key={index}
-            className={`flex ${
-              message.role === "user" ? "justify-end" : "justify-start"
-            }`}
+            className={`flex ${message.role === "user" ? "justify-end" : "justify-start"
+              }`}
           >
             <div
-              className={`max-w-xs px-4 py-2 rounded-lg ${
-                message.role === "user"
-                  ? "bg-blue-500 text-white"
-                  : "bg-gray-300 text-gray-800 dark:bg-gray-700 dark:text-white"
-              }`}
+              className={`max-w-xl px-4 py-2 rounded-lg ${message.role === "user"
+                ? "bg-blue-500 text-white"
+                : "bg-gray-300 text-gray-800 dark:bg-gray-700 dark:text-white"
+                }`}
             >
               {message.content}
             </div>
